0b6a2be9c1493cbfbd6a019cc570fe64f0bc484f
cgmattos <cgmattos98@gmail.com>
2022-02-22 08:59:13 -0300

    bispo funfa

--- Jogo/Bispo.java
+++ Jogo/Bispo.java
@@ -2,6 +2,7 @@ package Jogo;
 
 import Tabuleiro.PecaXadrez;
 import Tabuleiro.Tabuleiro;
+import Tabuleiro.Posicao;
 
 public class Bispo extends PecaXadrez {
 
@@ -25,6 +26,47 @@ public class Bispo extends PecaXadrez {
     @Override
     public boolean[][] movimentosPossiveis() {
         boolean[][] matriz = new boolean[getTabuleiro().getLinha()][getTabuleiro().getColuna()];
+
+        Posicao p = new Posicao(0,0);
+
+        p.setValores(posicao.getLinha()-1, posicao.getColuna()-1);                    //diagonal superior esquerda
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha()-1, p.getColuna()-1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && existePecaOponente(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+        }
+
+        p.setValores(posicao.getLinha()-1, posicao.getColuna()+1);                    //diagonal superior direita
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha()-1, p.getColuna()+1);
+
+        }
+        if (getTabuleiro().posicaoExiste(p) && existePecaOponente(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+        }
+
+        p.setValores(posicao.getLinha()+1, posicao.getColuna()-1);                    //diagonal inferior esquerda
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha()+1, p.getColuna()-1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && existePecaOponente(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+        }
+
+        p.setValores(posicao.getLinha()+1, posicao.getColuna()+1);                    //diagonal inferior direita
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+            p.setValores(p.getLinha()+1, p.getColuna()+1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && existePecaOponente(p)){
+            matriz[p.getLinha()][p.getColuna()] = true;
+        }
+
         return matriz;
+        
     }
 }
